5deb097500d404d5166b453602e5706478e466e6
wolfy <54258534+bethode@users.noreply.github.com>
2019-08-19 10:41:43 +0200

    Update wolfy.js

--- wolfy.js
+++ wolfy.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const token = 'NjEyNTI3NDYwMTU1OTE2Mjg4.XVjthg.c2QmZVhhKYUu0qDF-dgurnEh1xI';
+
 client.on('ready', () => {
     console.log('Ich bin Wolfy, raaaawr!');
 });
